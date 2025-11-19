8ff544c259eacac3913f10ccd0ccd8e1c0b200bf
s1010803 <s1010803@gm.pu.edu.tw>
2025-11-20 04:50:40 +0800

    Add backend practice file

--- data/products.js
+++ data/products.js
@@ -45,6 +45,10 @@ class Clothing extends Product {
   }
 }
 
+// const date = new Date();
+// console.log(date);
+// console.log(date.toLocaleTimeString());
+
 export const products = [
   {
     id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
